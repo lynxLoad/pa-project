44aadfaf405889b6b815abc4daaae0fcc246855d
zadzaza <85344142+zadzaza@users.noreply.github.com>
2025-04-25 12:17:03 +0300

    git add

---
+++ main/mask.go
@@ -0,0 +1,20 @@
+components {
+  id: "mask"
+  component: "/main/mask.script"
+}
+embedded_components {
+  id: "label"
+  type: "label"
+  data: "size {\n"
+  "  x: 128.0\n"
+  "  y: 32.0\n"
+  "}\n"
+  "text: \"Label\"\n"
+  "font: \"/builtins/fonts/default.font\"\n"
+  "material: \"/builtins/fonts/label-df.material\"\n"
+  ""
+  scale {
+    x: 0.2
+    y: 0.2
+  }
+}
