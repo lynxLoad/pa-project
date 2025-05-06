6a8c5cf2351addee06293de0ad67663add189c52
zadzaza <85344142+zadzaza@users.noreply.github.com>
2025-05-07 02:16:38 +0300

    git add

--- management/game_events.lua
+++ management/game_events.lua
@@ -2,6 +2,7 @@
 local event = require("event.event")
 local M = {}
 
+M.is_ui_mouse_hovered = false
 M.window_resized = event.create()
 
 return M
---
+++ ui/elements/button.lua
@@ -0,0 +1,31 @@
+local M = {}
+
+local game_events = require("management.game_events")
+
+function M:init()
+	self.root = self:get_node("root")
+
+	self.button = self.druid:new_button("btn", self.on_click)
+	self.button.on_hold_callback:subscribe(self.on_hold_callback)
+	self.hover = self.druid:new_hover("btn", self.on_hover_callback, self.on_mouse_hover_callback)
+	
+	self.text = self.druid:new_text("text", "Hello, Druid!")
+end
+
+function M:on_click()
+	self.text:set_text("The button clicked!")
+end
+
+function M:on_hold_callback()
+	self.text:set_text("The button clicked!")
+end
+
+function M:on_hover_callback()
+	--print(self.hover:is_hovered())
+end
+
+function M:on_mouse_hover_callback()
+	game_events.is_ui_mouse_hovered = self.hover:is_mouse_hovered()
+end
+
+return M
