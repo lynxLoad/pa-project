0e7ba6c59f2b2a119b20925c6d71e3cd4a8d9a36
zadzaza <85344142+zadzaza@users.noreply.github.com>
2025-05-29 01:46:53 +0300

    git add
    это ж надо такую хрень написать

---
+++ game/tilemap/map_config.lua
@@ -0,0 +1,8 @@
+local M = {}
+
+M.tilemap_path = "/tilemap"
+M.tile_size = 8
+M.width = 0
+M.height = 0
+
+return M
--- ui/elements/check_btn.lua
+++ ui/elements/check_btn.lua
@@ -1,19 +1,26 @@
 local M = {}
 
 local game_events = require("management.game_events")
+local event = require("event.event")
 
 function M:init()
 	self.root = self:get_node("root")
 
-	self.button = self.druid:new_button("btn", self.on_click)
-	self.button.on_hold_callback:subscribe(self.on_hold_callback)
-	self.hover = self.druid:new_hover("btn", self.on_hover_callback, self.on_mouse_hover_callback)
+	self.check_btn = self.druid:new_button("check_btn", self.on_click)
+	self.check_btn.on_hold_callback:subscribe(self.on_hold_callback)
+	self.hover = self.druid:new_hover("check_btn", self.on_hover_callback, self.on_mouse_hover_callback)
 	
 	self.text = self.druid:new_text("text", "Hello, Druid!")
+
+	self.active = false
+	self.on_active = event.create()
+	self.on_active:trigger(self.active)
 end
 
 function M:on_click()
 	self.text:set_text("The button clicked!")
+	self.active = not self.active
+	self.on_active:trigger(self.active)
 end
 
 function M:on_hold_callback()
